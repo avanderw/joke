e9ca1b4323e6599395ba4cdc7bcd10aa7ee82e01
Andrew van der Westhuizen <avanderw@gmail.com>
2022-10-12 23:43:43 +0200

    Add share button

--- joke.js
+++ joke.js
@@ -1,16 +1,32 @@
-let jokeCache;
+let jokesCache;
+let selectedJoke;
 fetch('jokes.txt')
 .then(response => response.text())
 .then(text => {
     const jokes = text.split('\n');
-    jokeCache = jokes;
+    jokesCache = jokes;
     getRandomJoke();
 })
 .catch(err => console.log(err));
 
 function getRandomJoke() {
-    const randomJoke = jokeCache[Math.floor(Math.random() * jokeCache.length)];
-    document.getElementById('joke').innerHTML = randomJoke;
+    selectedJoke = jokesCache[Math.floor(Math.random() * jokesCache.length)];
+    document.getElementById('joke').innerHTML = selectedJoke;
+}
+
+function shareJoke() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Joke',
+            text: selectedJoke,
+            url: window.location.href
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+        console.log('Web Share API not supported');
+    }
 }
 
 document.getElementById('jokeBtn').addEventListener('click', getRandomJoke);
+document.getElementById('shareBtn').addEventListener('click', shareJoke);
