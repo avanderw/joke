cf45d80786b00657fd718aa4c2cca21068f26030
Andrew van der Westhuizen <andrewvanderwesthuizen@capitecbank.co.za>
2024-08-27 12:05:17 +0200

    Add basic styling from base site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<a href="https://avanderw.co.za"><svg><use href="feather-sprite.svg#home" /></svg>Home</a>
+	<a href="https://github.com/avanderw/joke">
+		<svg><use href="feather-sprite.svg#github" /></svg>
+		Source
+	</a>
+  <a href="https://tracking.avanderw.co.za/avanderw.co.za">
+		<svg><use href="feather-sprite.svg#bar-chart-2" /></svg>
+		Analytics
+	</a>
+</div>
+
+<style>
+    h1 {
+      margin-top: 0;
+      padding-top: 0;
+    }
+      a {
+          display: inline-flex;
+          gap: 0.5rem;
+      }
+      div {
+          display: flex;
+          align-items: center;
+          justify-content: right;
+          gap: 2rem;
+      font-size: smaller;
+      }
+    svg {
+      width: 1rem;
+      height: 1rem;
+    }
+  </style>
