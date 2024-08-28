d89e6f7d22478ec5899d6e5810357de873a35dee
Andrew van der Westhuizen <avanderw@gmail.com>
2024-08-28 16:17:33 +0200

    Change precache strategy for PWA

---
+++ src/service-worker.js
@@ -0,0 +1,12 @@
+// https://www.sarcevic.dev/offline-first-installable-pwa-sveltekit-workbox-precaching
+// npm install -D workbox-precaching
+import { build, files, prerendered, version } from '$service-worker';
+import { precacheAndRoute } from 'workbox-precaching'
+
+const precache_list = [...build, ...files, ...prerendered].map((s) => ({
+	url: s,
+	revision: version,
+}))
+
+precacheAndRoute(precache_list)
+
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-        server: {
-                host: true, // used to allow traffic when in a container
-                watch: {
-                        usePolling: true // required for WSL/docker windows filesystem mount (CAUTION: CPU heavy)
-                }
-        },
-        plugins: [sveltekit()]
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  },
+  server: {
+    host: true, // used to allow traffic when in a container
+    watch: {
+      usePolling: true // required for WSL/docker windows filesystem mount (CAUTION: CPU heavy)
+    }
+  },
+  plugins: [sveltekit()]
 });
