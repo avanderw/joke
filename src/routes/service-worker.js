4b005fd8fd1cc404b44163ee3f819920fd1ec31b
Andrew van der Westhuizen <avanderw@gmail.com>
2024-08-28 15:59:08 +0200

    Add offline page

--- src/routes/service-worker.js
+++ src/routes/service-worker.js
@@ -70,9 +70,8 @@ self.addEventListener('fetch', (event) => {
 				return response;
 			}
 
-			// if there's no cache, then just error out
-			// as there is nothing we can do to respond to this request
-			throw err;
+			const cachedResponse = await cache.match("offline.html");
+			return cachedResponse;
 		}
 	}
 
